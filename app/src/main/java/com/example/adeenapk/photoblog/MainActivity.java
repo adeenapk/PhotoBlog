c7d47bd4851af8a107ed4226c030d934678f6e95
adeenapk <ahmedbahaaeldeenapk@gmail.com>
2018-11-28 22:18:03 +0200

    initial commit

---
+++ app/src/main/java/com/example/adeenapk/photoblog/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.adeenapk.photoblog;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        auth=FirebaseAuth.getInstance();
+
+
+        Intent intent=new Intent(MainActivity.this,LoginActivity.class);
+        startActivity(intent);
+        
+    }
+}
